d9412b81beb82760c926e25e536c1d1bda301264
tlsaudwl <tlsaudwl1821@naver.com>
2024-04-24 17:15:03 +0900

    initial setting

---
+++ src/test/java/com/study/kioskbackend/KioskBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.kioskbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KioskBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
