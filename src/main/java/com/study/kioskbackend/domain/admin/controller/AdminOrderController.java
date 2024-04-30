38417077658e2a4da9278e0d9060f23b3ccf429b
안나영 <dpswpfskdud@naver.com>
2024-04-30 12:50:13 +0900

    feat: AdminOrderController 생성

---
+++ src/main/java/com/study/kioskbackend/domain/admin/controller/AdminOrderController.java
@@ -0,0 +1,16 @@
+package com.study.kioskbackend.domain.admin.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/admin")
+public class AdminOrderController {
+
+
+
+
+}
